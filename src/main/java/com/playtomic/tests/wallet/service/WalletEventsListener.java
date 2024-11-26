21db579a4dd54ac2799485dfabe525d4ffeece49
bougueddach <bougueddach.anass@gmail.com>
2024-11-27 00:40:25 +0100

    feat: Use a local lock to handleWalletEntryCreatedEvent

--- src/main/java/com/playtomic/tests/wallet/service/WalletEventsListener.java
+++ src/main/java/com/playtomic/tests/wallet/service/WalletEventsListener.java
@@ -5,6 +5,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Component
 @AllArgsConstructor
@@ -12,13 +14,27 @@ public class WalletEventsListener {
 
     private final WalletRepository walletRepository;
     private final WalletEntryRepository walletEntryRepository;
+    private final ConcurrentHashMap<Long, ReentrantLock> walletLocks = new ConcurrentHashMap<>();
 
     /**
-     * In a distributed systems context I will use a kafka like event streaming platform instead
-     * For the sake of simplicity of the challenge I'm using local spring events
+     * 1- In a distributed systems context I will use a kafka like event streaming platform instead
+     *    For the sake of simplicity of the challenge I'm using local spring events
+     * 2- We could use Syncronised here instead but using locks gives a better visibility about how it'll work in a
+     *    distributed system by saving the locks in a distributed lock, with redis for example
      */
     @EventListener
     public void handleWalletEntryCreatedEvent(WalletEntryCreatedEvent event) {
+        ReentrantLock lock = walletLocks.computeIfAbsent(event.walletId, id -> new ReentrantLock());
+        lock.lock();
+        try {
+            snapshotWallet(event);
+        } finally {
+            lock.unlock();
+            walletLocks.remove(event.walletId);
+        }
+    }
+
+    private void snapshotWallet(WalletEntryCreatedEvent event) {
         Wallet wallet = walletRepository.findById(event.walletId)
                 .orElseThrow(() -> new ResourceNotFoundException("Wallet not found with id: " + event.walletId));
         List<WalletEntry> walletEntries = walletEntryRepository.findEventsAfter(event.walletId, wallet.getLastProcessedWalletEntry());
